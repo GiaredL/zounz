b1986e69ef8dc30f8cb28541d677614a7c470e63
Giared Mati <giared.mati@gmail.com>
2025-02-05 18:15:05 -0700

    added main centering div

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -1,46 +1,55 @@
-import { Header } from '../../layouts/features'
-import HeroSection from '../../components'
-import styles from './About.module.scss'
+import { Header } from "../../layouts/features";
+import HeroSection from "../../components";
+import styles from "./About.module.scss";
 
 const About = () => {
   return (
     <>
-      <div className={styles.about}>
-        <Header>
+      <Header>
+        <div className={styles.about}>
           <HeroSection>
-            <h1>Main purpose: help people discover artists, bands, and talents in their local music scene.</h1>
+            <h1>
+              Main purpose: help people discover artists, bands, and talents in
+              their local music scene.
+            </h1>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Description:</span> The user will log in and by default appear
-              on the charts page, which will display the top trending artists / bands in their selected area. Users
-              will be able to set their area or explore the map and see the local artists that are trending in that
-              area. Users will have their own profile, and will be able to upload music tracks for everyone's
-              listening pleasure. You can think of it like soundcloud, but the songs and artists that appear are
-              based on the local area. <br />
-              <br /> Users will be able to gain numbers in the charts by collecting streams through their music
-              profile. The app will have some of the following features:
+              <span style={{ fontWeight: "bold" }}>Description:</span> The user
+              will log in and by default appear on the charts page, which will
+              display the top trending artists / bands in their selected area.
+              Users will be able to set their area or explore the map and see
+              the local artists that are trending in that area. Users will have
+              their own profile, and will be able to upload music tracks for
+              everyone's listening pleasure. You can think of it like
+              soundcloud, but the songs and artists that appear are based on the
+              local area. <br />
+              <br /> Users will be able to gain numbers in the charts by
+              collecting streams through their music profile. The app will have
+              some of the following features:
               <br />
               <br />
               <ul>
                 <li>
-                  authentication and a user profile Charts page that shows the highest ranking artist within a
-                  selected area A community tab
+                  authentication and a user profile Charts page that shows the
+                  highest ranking artist within a selected area A community tab
                 </li>
-                <li>A timeline of local artist announcements / track releases</li>
+                <li>
+                  A timeline of local artist announcements / track releases
+                </li>
               </ul>
-              <p style={{ fontWeight: 'bold' }}>Possible features:</p>
+              <p style={{ fontWeight: "bold" }}>Possible features:</p>
               <ul>
                 <li>
-                  Integration with spotify API - allowing users to automatically rank based on their spotify
-                  popularity score.
+                  Integration with spotify API - allowing users to automatically
+                  rank based on their spotify popularity score.
                 </li>
                 <li>A world wide feature</li>
               </ul>
             </p>
           </HeroSection>
-        </Header>
-      </div>
+        </div>
+      </Header>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,20 +1,22 @@
-import { Header } from '../../layouts/features'
-import styles from './Home.module.scss'
-import HeroSection from '../../components/HeroSection'
+import { Header } from "../../layouts/features";
+import styles from "./Home.module.scss";
+import HeroSection from "../../components/HeroSection";
 
 const Home = () => {
   return (
     <>
       <div className={styles.home}>
         <Header>
-          <HeroSection
-            title="Hey, welcome to ZounZ, the place to discover local artists!"
-            description="Check back soon! In the meantime, try the about page."
-          />
+          <div className={styles["home"]}>
+            <HeroSection
+              title="Hey, welcome to ZounZ, the place to discover local artists!"
+              description="Check back soon! In the meantime, try the about page."
+            />
+          </div>
         </Header>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
