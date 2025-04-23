fc73162c48ee0a17669be6d3f68e59a27b4637d0
Giared Mati <giared.mati@gmail.com>
2025-04-22 20:23:27 -0600

    updated cors policy info

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 // Replace with your EC2's public IP or domain
-const API_URL = "http://54.241.113.130/api";
+const API_URL = "http://54.241.113.130:5000/api";
 
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // Important for session cookies
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
   },
 });
 
@@ -16,7 +19,7 @@ api.interceptors.response.use(
   (error) => {
     if (error.response?.status === 401) {
       // Handle unauthorized access (e.g., redirect to login)
-      window.location.href = "/login";
+      window.location.href = "/sign-in";
     }
     return Promise.reject(error);
   }
